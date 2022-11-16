47a8c9e0f4e5c1db6937627693c74a2b594d9f47
Sara Chen <k890244@gmail.com>
2022-11-16 13:51:31 +0800

    style: 刪減Attractions中多於的程式碼

--- modules/attractions.py
+++ modules/attractions.py
@@ -16,105 +16,54 @@ class Attractions(Resource):
             response.headers["Access-Control-Allow-Origin"] = "*"
             return response
 
-        if keyword == "":
-            try:
-                connection = db_Connect.dbConnect.get_connection()
-                cursor = connection.cursor()
-                cursor.execute("SELECT COUNT(id) FROM attractions")
-                totalID = cursor.fetchone()
-                cursor.execute("SELECT * FROM attractions LIMIT %s, 12", [page*12])
-                results = cursor.fetchall()
+        try:
+            connection = db_Connect.dbConnect.get_connection()
+            cursor = connection.cursor()
+            cursor.execute("SELECT COUNT(id) FROM attractions WHERE category = %s OR name LIKE %s",[keyword, f"%{keyword}%"])
+            totalID = cursor.fetchone()
+            cursor.execute("SELECT * FROM attractions WHERE category = %s OR name LIKE %s LIMIT %s, 12",[keyword, f"%{keyword}%", page*12])
+            results = cursor.fetchall()
 
-                data = []
-                for result in results:
-                    data_group = {"id": result[0],
-                            "name": result[1],
-                            "category": result[2],
-                            "description": result[3],
-                            "address": result[4],
-                            "transport": result[5],
-                            "mrt": result[6],
-                            "lat": result[7],
-                            "lng": result[8],
-                            "images": json.loads(result[9])
-                        }
-                    data.append(data_group)
+            data = []
+            for result in results:
+                data_group = {"id": result[0],
+                        "name": result[1],
+                        "category": result[2],
+                        "description": result[3],
+                        "address": result[4],
+                        "transport": result[5],
+                        "mrt": result[6],
+                        "lat": result[7],
+                        "lng": result[8],
+                        "images": json.loads(result[9])
+                    }
+                data.append(data_group)
 
-                if page < totalID[0]/12-1: 
-                    response = jsonify({
-                    "nextPage": page+1,
-                    "data": data
-                    })
-                    response.headers["Content-Type"] = "application/json"
-                    response.headers["Access-Control-Allow-Origin"] = "*"
-                    return response
-                elif page >= totalID[0]//12:
-                    response = jsonify({
-                    "nextPage": None,
-                    "data": data
-                    })
-                    response.headers["Content-Type"] = "application/json"
-                    response.headers["Access-Control-Allow-Origin"] = "*"
-                    return response
-            except:
-                response = jsonify({"error": True,"message": "server error"})
-                response.status_code = "500"
+            if page < totalID[0]/12-1: 
+                response = jsonify({
+                "nextPage": page+1,
+                "data": data
+                })
                 response.headers["Content-Type"] = "application/json"
                 response.headers["Access-Control-Allow-Origin"] = "*"
                 return response
-            finally:
-                cursor.close()
-                connection.close()
-
-        elif keyword != "":
-            try:
-                connection = db_Connect.dbConnect.get_connection()
-                cursor = connection.cursor()
-                cursor.execute("SELECT COUNT(id) FROM attractions WHERE category = %s OR LOCATE (%s, name)",[keyword, keyword])
-                totalID = cursor.fetchone()
-                cursor.execute("SELECT * FROM attractions WHERE category = %s OR LOCATE (%s, name) LIMIT %s, 12",[keyword, keyword, page*12])
-                results = cursor.fetchall()
-
-                data = []
-                for result in results:
-                    data_group = {"id": result[0],
-                            "name": result[1],
-                            "category": result[2],
-                            "description": result[3],
-                            "address": result[4],
-                            "transport": result[5],
-                            "mrt": result[6],
-                            "lat": result[7],
-                            "lng": result[8],
-                            "images": json.loads(result[9])
-                        }
-                    data.append(data_group)
-
-                if page < totalID[0]/12-1: 
-                    response = jsonify({
-                    "nextPage": page+1,
-                    "data": data
-                    })
-                    response.headers["Content-Type"] = "application/json"
-                    response.headers["Access-Control-Allow-Origin"] = "*"
-                    return response
-                elif page >= totalID[0]//12:
-                    response = jsonify({
-                    "nextPage": None,
-                    "data": data
-                    })
-                    response.headers["Content-Type"] = "application/json"
-                    response.headers["Access-Control-Allow-Origin"] = "*"
-                    return response
-            except:
-                response = jsonify({"error": True,"message": "server error"})
-                response.status_code = "500"
+            elif page >= totalID[0]//12:
+                response = jsonify({
+                "nextPage": None,
+                "data": data
+                })
                 response.headers["Content-Type"] = "application/json"
                 response.headers["Access-Control-Allow-Origin"] = "*"
                 return response
-            finally:
-                cursor.close()
-                connection.close()
+        except:
+            response = jsonify({"error": True,"message": "server error"})
+            response.status_code = "500"
+            response.headers["Content-Type"] = "application/json"
+            response.headers["Access-Control-Allow-Origin"] = "*"
+            return response
+        finally:
+            cursor.close()
+            connection.close()
 
 # 根據景點編號取得景點資料
 class Search_Attractions(Resource):
