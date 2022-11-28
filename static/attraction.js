23efb85a494b8d0e6b1b652f15e33478b9440d4f
Sara Chen <k890244@gmail.com>
2022-11-29 01:04:37 +0800

    feat: 新增景點圖片輪播功能

--- static/attraction.js
+++ static/attraction.js
@@ -7,6 +7,10 @@ const morning = document.querySelector(".morning");
 const afternoon = document.querySelector(".afternoon");
 const price = document.querySelector(".price-1");
 
+const slidesPictures = document.querySelector(".slides-pictures");
+const dotPosition = document.querySelector(".dot-position");
+let slideIndex = 0;
+
 let path = location.pathname;
 // console.log(path); //印出/attraction/1
 
@@ -23,6 +27,23 @@ function getData() {
       description.textContent = result.description;
       address.textContent = result.address;
       transport.textContent = result.transport;
+
+      let pictureString = "";
+      let dotString = "";
+      for (let i = 0; i < result.images.length; i++) {
+        pictureString += `
+        <div class=slide><img src=${result.images[i]} /></div>
+        `;
+
+        dotString += `
+        <div class="dot" onclick="currentSlide(${i})"></div>
+        `;
+      }
+
+      slidesPictures.innerHTML = pictureString;
+      dotPosition.innerHTML = dotString;
+
+      showSlides(slideIndex);
     });
 }
 
@@ -33,3 +54,40 @@ morning.addEventListener("click", function () {
 afternoon.addEventListener("click", function () {
   price.textContent = "新台幣 2500 元";
 });
+
+//下一張
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+//圖片圓點
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+//顯示圖片
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".slide");
+  let dots = document.querySelectorAll(".dot");
+
+  //超過輪播圖片的數量，回到第一張
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  //小於第一張，回到最後一張
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active"; //使用+=才不會覆蓋掉原本的class
+}
