086a51724ee494bc22f2bff8cbce221a4d60a989
Sara Chen <k890244@gmail.com>
2022-12-22 15:12:42 +0800

    feat: 訂購完成頁面及連至訂單頁面

---
+++ static/javascript/thankyou.js
@@ -0,0 +1,35 @@
+const path = location.href;
+const orderNumber = path.split("=")[1];
+const orderMessage = document.querySelector(".order-message");
+const orderNumberText = document.querySelector(".order-number");
+const orderPage = document.querySelector(".order-page");
+
+window.onload = async () => {
+  await checkSigninStatus();
+  if (signinStatus) {
+    loadData();
+  } else {
+    document.location.href = "/";
+  }
+};
+
+async function loadData() {
+  try {
+    const response = await fetch(`/api/order/${orderNumber}`);
+    const data = await response.json();
+    const result = data.data;
+    if (result) {
+      orderNumberText.textContent = orderNumber;
+      orderPage.href = `/order/${orderNumber}`;
+    } else {
+      document.location.href = "/";
+    }
+    if (result.status == "付款成功") {
+      orderMessage.textContent = "行程預定成功";
+    } else {
+      orderMessage.textContent = "行程預定失敗";
+    }
+  } catch (error) {
+    console.log("error", error);
+  }
+}
